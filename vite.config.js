7521a7dfc98dc2ce012362688fd82957a656f836
Luisa Bustamante <94744814+LuisaBustamante@users.noreply.github.com>
2024-08-15 19:24:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Reactjs-deploy-githubpages/', // Update this to your GitHub Pages URL
+  base: 'https://luisabustamante.github.io/SistemaNotas/', // Update this to your GitHub Pages URL
 })
